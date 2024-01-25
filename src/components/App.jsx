e59cbcab65eea6604c9770011032b9cf80c9fa7b
Ivan <ivanvasyunin@gmail.com>
2024-01-25 16:21:47 +0100

    initial files added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import EditEventPage from '../pages/EditEvent';
+import ErrorPage from '../pages/Error';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from '../pages/EventDetail';
+import EventsPage, { loader as eventsLoader } from '../pages/Events';
+import EventsRootLayout from '../pages/EventsRoot';
+import HomePage from '../pages/Home';
+import NewEventPage from '../pages/NewEvent';
+import RootLayout from '../pages/Root';
+import { action as manipulateEventAction } from './EventForm';
+import NewsletterPage, { action as newsletterAction } from '../pages/Newsletter';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: 'events',
+        element: <EventsRootLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: ':eventId',
+            id: 'event-detail',
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
+          },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+        ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
